79f92993fa3fb53754dae6046d92cd728ee6b3ec
Larry Wen <lwen68831@gmail.com>
2025-11-22 15:56:24 -0500

    end of section 12 - 92

--- YumBlazor/Data/ApplicationDbContext.cs
+++ YumBlazor/Data/ApplicationDbContext.cs
@@ -5,5 +5,17 @@ namespace YumBlazor.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<Category> Category { get; set; }    // add Category to table
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);  // do not remove, it is required in order to seed category table
+
+            builder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Appetizers" },
+                new Category { Id = 2, Name = "Entree" },
+                new Category { Id = 3, Name = "Dessert" }
+            );
+        }
     }
 }
