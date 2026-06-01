f2dced4992dc073ab311af0372cc4806561abebc
Larry Wen <lwen@pcrb.com>
2026-05-31 21:11:04 -0400

    end of Section 20:Show Cart Counter

---
+++ YumBlazor/Services/SharedStateService.cs
@@ -0,0 +1,20 @@
+﻿namespace YumBlazor.Services
+{
+    public class SharedStateService
+    {
+        public event Action OnChange; //notify subscribers when a state is updated
+        private int _totalCartCount;
+
+        public int TotalCartCount
+        {
+            get => _totalCartCount;
+            set
+            {
+                _totalCartCount = value;
+                NotifyStateChanged();
+            }
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
